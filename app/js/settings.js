91ac36f333926dbe0175a3dd7c36796622ff1b89
vjrj <vjrj@comunes.org>
2025-05-13 14:19:05 +0200

    Update mainLAUrl, baseFooterUrl, and sentryUrl in settings.js

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,11 +1,12 @@
 export default {
   isDevel: true,
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
-  enabledLangs: ['en', 'es', 'ca'],
+  enabledLangs: ['es', 'en', 'ca'],
   mainDomain: 'gbif.es', // used for cookies (without http/https)
-  mainLAUrl: 'https://gbif.es',
-  baseFooterUrl: 'https://gbif.es',
+  mainLAUrl: 'https://datos.gbif.es/',
+  baseFooterUrl: 'https://datos.gbif.es/brand-2025-vite',
   theme: 'gbifes', // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
+  sentryUrl: "https://a769ac65ea144535bfe30afcbc431ef4@sentry.comunes.org/2",
   services: {
     collectory: { url: 'https://colecciones.gbif.es', title: 'Collections' },
     biocache: { url: 'https://registros.gbif.es', title: 'Occurrence records' },
@@ -15,14 +16,14 @@ export default {
     },
     bie: { url: 'https://especies.gbif.es', title: 'Species' },
     bieService: {
-      url: 'https://registros-ws.gbif.es',
+      url: 'https://especies-ws.gbif.es',
       title: 'Species webservice',
     },
     regions: { url: 'https://regiones.gbif.es', title: 'Regions' },
     lists: { url: 'https://listas.gbif.es', title: 'Species List' },
     spatial: { url: 'https://espacial.gbif.es', title: 'Spatial Portal' },
     images: { url: 'https://imagenes.gbif.es', title: 'Images Service' },
-    cas: { url: 'https://auth.gbif.es', title: 'CAS' },
+    cas: { url: 'https://auth.gbif.es', title: 'CAS' }
   },
   otherLinks: [
     { title: 'Datasets', url: 'https://colecciones.gbif.es/datasets' },
@@ -31,7 +32,7 @@ export default {
       url: 'http://registros.gbif.es/explore/your-area/',
     },
     { title: 'Datasets', url: 'https://colecciones.gbif.es/datasets' },
-    { title: 'twitter', url: '', icon: 'twitter' },
-  ],
+    { title: 'twitter', url: 'https://twitter.com/GbifEs', icon: 'twitter' }
+  ]
 };
 
