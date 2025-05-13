86c178ceb87bd17e4c8273a46f76284dd6c712ae
vjrj <vjrj@comunes.org>
2025-05-13 17:36:32 +0200

    Add new font sources for Open Sans and Noto Serif

--- app/js/init.js
+++ app/js/init.js
@@ -5,3 +5,7 @@ import './mante.js';
 import './stats.js';
 import './autocomplete-conf.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/noto-serif/400.css';
+import '@fontsource/noto-serif/600.css';
