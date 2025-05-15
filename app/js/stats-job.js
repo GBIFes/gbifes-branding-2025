9fcc0d482e291bb282591745a6ee682b85e19d38
vjrj <vjrj@comunes.org>
2025-05-15 09:04:17 +0200

    Refactor stats-job.js to use ES6 imports and async-await

--- app/js/stats-job.js
+++ app/js/stats-job.js
@@ -1,8 +1,10 @@
-const settings = require('./settings');
+import settings from './settings.js';
+import bent from 'bent';              // opcional: podrías usar fetch
+
 const collectory = settings.services.collectory.url;
 const biocacheService = settings.services.biocacheService.url;
-const bent = require('bent')
-const getJSON = bent('json')
+
+const getJSON = bent('json');
 
 // If you want to show collections stats:
 // `${collectory}/ws/dataResource/count`
@@ -10,15 +12,15 @@ const getJSON = bent('json')
 let loadStats = async () => {
   let stats = {};
   let data = await getJSON(`${biocacheService}/occurrences/search`);
-  stats["records"] = data.totalRecords;
+  stats['records'] = data.totalRecords;
   data = await getJSON(`${collectory}/ws/dataResource/count`);
-  stats["drs"] = data.total;
+  stats['drs'] = data.total;
   data = await getJSON(`${collectory}/ws/institution/count`);
-  stats["institutions"] = data.total;
+  stats['institutions'] = data.total;
   // Right now this is slow
   data = await getJSON(`${biocacheService}/occurrence/facets?q=*:*&facets=species&pageSize=0`);
-  stats["species"] = data[0].count;
+  stats['species'] = data[0].count;
   console.log(JSON.stringify(stats));
-}
+};
 
 loadStats();
