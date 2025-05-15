0d052a625981121b73387d3c2eb4c9ab7666d3dd
vjrj <vjrj@comunes.org>
2025-05-15 09:04:07 +0200

    demo settings

--- app/js/settings.js
+++ app/js/settings.js
@@ -3,8 +3,8 @@ export default {
   inMante: false, // set to true and deploy if you want to set a maintenance message in all the services
   enabledLangs: ['es', 'en', 'ca'],
   mainDomain: 'gbif.es', // used for cookies (without http/https)
-  mainLAUrl: 'https://datos.gbif.es/',
-  baseFooterUrl: 'https://datos.gbif.es/brand-2025-vite',
+  mainLAUrl: 'https://demo.gbif.es/',
+  baseFooterUrl: 'https://demo.gbif.es/brand-2025-vite/',
   theme: 'gbifes', // for now 'material', 'clean', 'superhero', 'yeti', 'cosmo', 'darkly', 'paper', 'sandstone', 'simplex', 'slate' or 'flatly' themes are available. See the last ones in: https://bootswatch.com/3/
   sentryUrl: "https://a769ac65ea144535bfe30afcbc431ef4@sentry.comunes.org/2",
   services: {
