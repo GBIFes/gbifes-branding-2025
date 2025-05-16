0db560391dcb21ff6e5454a52b4dec0c0574c78f
vjrj <vjrj@comunes.org>
2025-05-16 22:22:37 +0200

    Add polyfill for URL() usage and import new scripts

--- app/js/init.js
+++ app/js/init.js
@@ -4,6 +4,7 @@ import './i18next-config.js';
 import './mante.js';
 import './stats.js';
 import './autocomplete-conf.js';
+import 'url-polyfill'; // for URL() usage in older browsers
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fontsource/open-sans/400.css';
 import '@fontsource/open-sans/600.css';
@@ -11,3 +12,9 @@ import '@fontsource/noto-serif/400.css';
 import '@fontsource/noto-serif/600.css';
 // Disable the use of gbif.es css/sass for now
 // import '../css/main.scss';
+import './collectory.js';
+import './sentry.js';
+import './tooltip.js';
+import './top-search.js';
+import './i18n_init.js';
+import './i18n_menus.js';
